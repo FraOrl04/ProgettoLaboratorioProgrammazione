6f686163081e2df93530062d73bbbce84a3ab451
FraOrl04 <166861346+FraOrl04@users.noreply.github.com>
2024-09-23 18:04:59 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/Test/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fra/CLionProjects/ProgettoLaboratorioProgrammazione/Test/lib/googletest/googletest
-# Build directory: /home/fra/CLionProjects/ProgettoLaboratorioProgrammazione/cmake-build-debug/Test/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
